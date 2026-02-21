2f4d895f9806bd0282f824ce301cc77717cbdfc8
ssverchkov <ssverchkov@tango.me>
2026-02-21 23:04:54 +0100

    add board M5AtomS3

--- src/board/BoardSelector.h
+++ src/board/BoardSelector.h
@@ -4,6 +4,8 @@
     #include "esp32devkit/Pins.h"
 #elif defined(BOARD_M5_NANOC6)
     #include "m5nanoc6/Pins.h"
+#elif defined(BOARD_M5_ATOMS3)
+    #include "m5atoms3/Pins.h"
 #else
     #error "Board not defined!"
 #endif
---
+++ src/board/m5atoms3/Pins.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Pins {
+    constexpr int LED = 35;
+    constexpr int BUTTON = 41;
+    constexpr int LED_R = 1;
+    constexpr int LED_G = 2;
+    constexpr int LED_B = 3;
+}
